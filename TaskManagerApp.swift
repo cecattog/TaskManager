2ae0ecf289b7139d1ef7fbb8ef05b7b84fb8f7ea
Guilherme Serpa <guilhermececatto@icloud.com>
2026-05-04 18:32:42 +0200

    Started the project

---
+++ TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Guilherme Serpa Cecatto on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
